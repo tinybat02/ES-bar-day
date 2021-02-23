ae98631e106c4c9dcfb08ed73d1fb5bbaee148cd
Nam Le <nam.leduc02@gmail.com>
2021-02-23 20:54:59 +0100

    1st commit

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+import { DataFrame, Field, Vector } from '@grafana/data';
+
+export interface PanelOptions {
+  timezone: string;
+  barOrder: string[];
+}
+
+export const defaults: PanelOptions = {
+  timezone: 'Europe/Berlin',
+  barOrder: [],
+};
+
+export interface Buffer extends Vector {
+  buffer: number[];
+}
+
+export interface FieldBuffer extends Field<any, Vector> {
+  values: Buffer;
+}
+
+export interface Frame extends DataFrame {
+  fields: FieldBuffer[];
+}
